f67560da633f78ab44d92a2d273ead585424ee54
Takuto <cguj8053@mail4.doshisha.jp.ac>
2025-12-15 20:07:58 +0900

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,26 @@
+Rails.application.routes.draw do
+  devise_for :users
+
+  resources :users, only: [:show]
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
+  get 'hello/index' => 'hello#index'
+  get 'hello/link' => 'hello#link'
+
+  Rails.application.routes.draw do
+    resources :tweets do
+      resources :likes, only: [:create, :destroy]
+    end
+  end
+  
+  root 'hello#index'
+
+ 
+end
+
+  # Defines the root path route ("/")
+  # root "posts#index"
+
